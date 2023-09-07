cc072fabfca03586a029300da49ec628535dd3f0
Andrea Alvarez <andreavaleria.951@gmail.com>
2023-09-07 15:13:47 -0300

    Intento final

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-document.getElementById("enviarBtn").addEventListener("click", saveDataData);
+document.addEventListener("DOMContentLoaded", (event) => {
 
-function saveData() {
-    element.getElementById("formulario").
-}
+const formulario = document.getElementById('formulario');
+const botonEnviar = document.getElementById('enviar');
 
-const data = document.getElementById("Formulario");
-fetch("https://jsonplaceholder.typicode.com/users", {
-    method: "POST",
-    body: data
-})
-.then(function(response) {
-    if(response.ok) {
-        return response.text()
-    } else {
-        throw "Error en la llamada"
-    }
-})
-.then(function(texto) {
-    console.log(texto);
-})
-.catch(function(err) {
-    console.log(err);
+botonEnviar.addEventListener('click', () => {
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const fechaDeNacimiento = document.getElementById('nacimiento').value;
+    const datos = {
+        nombre: nombre,
+        apellido: apellido,
+        fechaDeNacimiento: fechaDeNacimiento
+    };
+
+    fetch('https://jsonplaceholder.typicode.com/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+    })
+    .then(response => response.json())
+    .then(datos => {
+        console.log('Respuesta del servidor:', datos);
+        alert('Datos enviados con éxito');
+    })
+    .catch(error => {
+        console.error('Error al enviar los datos:', error);
+        alert('Error al enviar los datos');
+    });
+});
 });
